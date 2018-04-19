089bd391b2e84477a16264d2600bbe343dee9cc2
nick <nick@ch71s8dev234.honeywell.com>
2018-04-19 11:51:35 -0400

    Create asp.net .net core app with message customized

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {	
+
+            var host = BuildWebHost(args);
+			host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) {
+			var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
+			var url = config["ASPNETCORE_URLS"]?? "http://*:8080";
+
+            return new WebHostBuilder().UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+				.UseUrls(url)
+                .Build();
+		}
+    }
+}
